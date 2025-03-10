4962ab914da114e642166f88533e0d55562d5a05
LavenderMorara <chiyamorara@gmail.com>
2025-03-10 17:10:57 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, event){
+    let messages = []
+    for (let n=0; n<names.length; n++){
+       messages.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+writeCards(names, "surprise")
+
+function countDown(){
+  let n = 10;
+  while(n<=10&&n>=0){
+    console.log(n)
+    n--
+  }
+}
